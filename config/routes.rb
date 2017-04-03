b05f34f74a1ca23cf734cb24321c73c84edee23f
Kerry <kerrysng@gmail.com>
2017-04-03 22:05:34 +1000

    user crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+get '/signup', to: 'user#new'
+post '/user', to: 'user#create'
+
+get '/login', to: 'session#new'
+post '/session', to: 'session#create'
+delete '/session', to: 'session#destroy'
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 20170403072849) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email"
+    t.string   "email", :default: '', :null: false
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
