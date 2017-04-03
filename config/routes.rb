02f907c4412045f510d1385fe72cdf3591a25ef5
Kerry <kerrysng@gmail.com>
2017-04-03 22:23:51 +1000

    card crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'cards/new'
+
   get 'sessions/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -9,6 +11,13 @@ post '/user', to: 'user#create'
 get '/login', to: 'session#new'
 post '/session', to: 'session#create'
 delete '/session', to: 'session#destroy'
+
+get '/cards', to: 'cards#index'
+get '/cards/new', to: 'cards#new' #new is method for getting a form
+
+post '/cards', to: 'cards#create'
+delete '/cards', to: 'cards#destroy'
+get '/cards/:id', to: 'cards#show'
 
 
 end
