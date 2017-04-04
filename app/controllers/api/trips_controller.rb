a94d06f245283918d0dcdeaf0895c4ec88be0054
Kerry <kerrysng@gmail.com>
2017-04-04 15:30:04 +1000

    fixed trip

--- app/controllers/api/trips_controller.rb
+++ app/controllers/api/trips_controller.rb
@@ -1,5 +1,7 @@
 class Api::TripsController < ApplicationController
   def index
+    render json: Trip.all
+
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   post '/new', to: 'pages#create_user'
   get '/new_card_form', to: 'pages#new_card_form'
 
+
   namespace :api do
     resources :trips , :cards
   end
