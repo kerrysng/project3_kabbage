105f4d95e20c11e1a6865e13ffb0e3008bb73057
Gerard Ho <gerard.ho@live.com.au>
2017-04-04 21:13:55 +1000

    completed new cards

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,6 +3,7 @@ class SessionController < ApplicationController
   def create_session
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
+      session[:id] = user.id
       session[:first_name] = user.first_name
       redirect_to '/dashboard'
     else
