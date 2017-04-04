04a5e3fed3dd1822d6daecbbee0d2245046e9f76
anikoh <anikoh@gmail.com>
2017-04-04 15:43:22 +1000

    cards backbone template

--- app/assets/javascripts/CardItemView.js
+++ app/assets/javascripts/CardItemView.js
@@ -1,43 +1,42 @@
 // view
-
-
 var CardItemView = Backbone.View.extend({
 
   className: 'card',
 
-  // changing the root element - tag name/ adding classes
-  //tagName: '<span>',
-  //className:
-
-  template: Handlebars.compile($('#card-template').html()),
-
-  events: {
-    'click': 'hardify',
-    'keyup input': 'save'
+  template: function() {
+    return Handlebars.compile($('#card-template').html())
   },
 
-// new QuoteItemView calls initialize
-// in this case 'this' acts like self
+  events: {
+    'click .edit': 'edit',
+  //  'keyup input': 'save'
+    'hover': 'expand'
+  },
+
   initialize: function(){
     this.listenTo(this.model, 'change', this.render);
   },
 
 
-  save: function(event){
-    if (event.which === 13){
-      this.model.set('body', event.target.value);
- //console.log(event.target);
-    }
+  expand: function(){
+    this.$el.css("border", "#red solid 1px");
   },
 
-  hardify: function(){
-    this.$el.find('input').show();
-    this.$el.find('h2').hide();
-  },
+ //  save: function(event){
+ //    if (event.which === 13){
+ //      this.model.set('body', event.target.value);
+ // //console.log(event.target);
+ //    }
+ //  },
+ //
+ //  hardify: function(){
+ //    this.$el.find('input').show();
+ //    this.$el.find('h2').hide();
+ //  },
 
   render: function(){
-    var html = this.template(this.model.toJSON());
+    var html = this.template()(this.model.toJSON());
     this.$el.html(html);
     return this;
   }
-})
+});
