cc76719caeacceba82f8f3df5bf57bc5fa3426de
Gerard Ho <gerard.ho@live.com.au>
2017-04-05 22:28:53 +1000

    card submit on trip page

--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -1,3 +1,61 @@
-$('#create_new_card').on('click',function(){
-  
+$(document).ready(function(){
+  $('#create_new_card').on('click',function(){
+    var $newForm = $('#new_card_container').append(createForm());
+    geoLocation();
+  });
+
+  function createForm(){
+    var $form = $('<form>').attr('class','new-card-form');
+    var $input1 = $('<input>').attr('type','text').attr('name','lat').attr('id','latitude');
+    var $input2 = $('<input>').attr('type','text').attr('name','long').attr('id','longitude');
+    var $input3 = $('<input>').attr('type','text').attr('name','country').attr('id','country');
+    var $input4 = $('<input>').attr('type','text').attr('name','city').attr('id','city');
+    var $input5 = $('<input>').attr('type','text').attr('name','trip_id').attr('id','tripId');
+    var $label1 = $('<label>').text("Image URL");
+    var $input6 = $('<input>').attr('type','text').attr('name','image_url');
+    var $label2 = $('<label>').text("Description");
+    var $input7 = $('<input>').attr('type','text').attr('name','body');
+    var $button = $('<button>').text("Submit");
+    return $form.append($input1).append($input2).append($input3).append($input4).append($input5).append($label1).append($input6).append($label2).append($input7).append($button);
+  }
+
+  function geoLocation(){
+    initiate_geolocation();
+    function initiate_geolocation() {
+      navigator.geolocation.getCurrentPosition(handle_geolocation_query);
+    }
+
+    function handle_geolocation_query(position){
+      var geocoder = new google.maps.Geocoder;
+      var infowindow = new google.maps.InfoWindow;
+      var a = position.coords.latitude.toFixed(6);
+      var b = position.coords.longitude.toFixed(6);
+      var latlng = {lat: parseFloat(a), lng: parseFloat(b)};
+      $('#latitude').val(a);
+      $('#longitude').val(b);
+      $('#tripId').val($('#trip_id').val());
+      geocoder.geocode({'location': latlng}, function(results, status) {
+        var locationArray = results[5].formatted_address.split(", ");
+        $('#country').val(locationArray[2]);
+        $('#city').val(locationArray[0]);
+      })
+    }
+    
+    $('.new-card-form').on('submit',function(event){
+      event.preventDefault();
+      console.log("hello this button is working")
+      $.ajax({
+        url: '/api/cards',
+        method: 'post',
+        data: $('.new-card-form').serialize()
+      }).done(function(data){
+        console.log(data)
+        // var source = $('#card-template').html();
+        // var templateFunction = Handlebars.compile(source);
+        // var html = templateFunction(data);
+        // $('.wrapper').append(html);
+        // button();
+      })
+    })
+  }
 })
