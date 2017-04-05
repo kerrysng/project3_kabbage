42e50645d881d133cd2e9e53ced5ee2b344bf28d
Gerard Ho <gerard.ho@live.com.au>
2017-04-05 16:56:15 +1000

    work on trip

--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -1,21 +1,3 @@
-var QuoteItemView = Backbone.View.extend({
-  template: Handlebars.compile($('#new_card_form').html()),
-  events: {
-    'click .new_card_action': 'showCardForm'
-  },
-  showCardForm: function(){
-    this.$el.find('p').css('text-decoration','line-through');
-  },
-  render: function(){
-    // var source = $('#quote-item-template').html()
-    // var template = Handlebars.compile(source);
-    var html = this.template(this.model);
-    this.$el.html(html);
-  }
-});
-
 $('#create_new_card').on('click',function(){
-  var view = new QuoteItemView()
-  view.render();
-  $('#new_card_container').append(view.el)
+  
 })
