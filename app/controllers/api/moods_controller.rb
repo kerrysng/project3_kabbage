54b6928d3e37071f329b0b34d640e51e3a0d1d6e
Barry Tabios <b_tabios@yahoo.com>
2017-04-06 13:47:12 +1000

    update

--- app/controllers/api/moods_controller.rb
+++ app/controllers/api/moods_controller.rb
@@ -1,24 +1,8 @@
-
 class Api::MoodsController < ApplicationController
 
-
   def index
-    @moods = Mood.all
-
+    render json: Mood.all
   end
-
-  def show
-  end
-
-  def new
-    @mood = current_user.moods.build
-
-  end
-
-  def edit
-    
-  end
-
 
   def create
     mood = Mood.new
@@ -29,42 +13,6 @@ class Api::MoodsController < ApplicationController
     redirect_to '/'
   end
 
-  # def index
-  #   @moods = Mood.all
-  #
-  # end
-
-  # def show
-  # end
-
-
-
-  # def edit
-  #
-  # end
-
-  # def create
-  #   @mood = current_user.moods.build(mood_params)
-  #   if @mood.save
-  #     redirect_to @mood
-  #   else
-  #     render :new
-  #   end
-  # end
-  #
-  # def update
-  # end
-  #
-  # def destroy
-  #   @mood.destroy
-  #   redirect_to moods_url
-  # end
-  #
-  #
-  #
-  # def mood_params
-  #   params.permit(:card_id, :trip_id)
-  # end
 
 
 
